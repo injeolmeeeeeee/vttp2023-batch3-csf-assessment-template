d44d1999fc1222e5771fc4792bfe3a8033c91a0f
injeolmeeeeeee <86404260+injeolmeeeeeee@users.noreply.github.com>
2024-02-28 14:45:12 +0800

    commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'vttp2023-batch3-csf-assessment-template';
+}
